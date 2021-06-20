ee6c5ee0535617cf911d6ce4e8a7c80af7f2aee9
Max <m412tinez@gmail.com>
2021-06-20 18:45:28 -0500

    created function to fetch data from openweather one call api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,17 @@ $(searchBut).on('click', function (event) {
     var geoAPI= 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchedCity + '&appid=e16523d04c63d1ae7214ce72c3259465';
     fetch(geoAPI)
         .then(response => response.json())
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
-            var long= data[0].lon;
-            var lat= data[0].lat;
-            var searchWeath = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid' + APIKey;
+            var long = data[0].lon;
+            var lat = data[0].lat;
+            var searchWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid=' + APIKey;
             $(city).text(data[0].name + ', ' + data[0].state)
+
+            fetch(searchWeather)
+                .then(response => response.json())
+                .then(function (data) {
+                    console.log(data);
+                })
         })
 })
