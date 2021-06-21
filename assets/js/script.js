46781183839d5b115c93b21ee527261bef784a00
Max <m412tinez@gmail.com>
2021-06-20 21:02:51 -0500

    completed for loop working on city history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var searchBut= $("#searchBtn");
 var APIKey= 'e16523d04c63d1ae7214ce72c3259465'
 var city= $('#city')
-const icons= $('#currentIcon')
+const icon= $('#currentIcon')
 const temp= $('#temp');
 const wind= $('#wind');
 const humid= $('#humid');
@@ -15,6 +15,9 @@ $(searchBut).on('click', function (event) {
         .then(response => response.json())
         .then(function (data) {
             console.log(data);
+
+            localStorage.setItem('city', JSON.stringify(searchedCity));
+
             var long = data[0].lon;
             var lat = data[0].lat;
             var WeatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid=' + APIKey;
@@ -24,15 +27,30 @@ $(searchBut).on('click', function (event) {
                 .then(response => response.json())
                 .then(function (data) {
                     console.log(data);
-                    $(icons).attr("src", 'https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png');
+                    $(icon).attr("src", 'https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png');
                     $(uv).text('UV index: ' + data.current.uvi);
                     $(temp).text('Temp: ' + data.current.temp + '\u00B0' + 'F');
                     $(wind).text('Wind: ' + data.current.wind_speed + 'MPH');
                     $(humid).text('Humidity: ' + data.current.humidity + '%');
 
                 for(i = 1; i < 6; i++) {
-                    var 
+                  icon[i]= document.getElementById('icon' + i);
+                    $(icon[i]).attr("src", 'https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png');
+                  temp[i]= document.getElementById('temp' + i);
+                    $(temp[i]).text('Temp: ' + data.daily[i].temp.day + '\u00B0' + 'F');
+                  wind[i]= document.getElementById('wind' + i);
+                    $(wind[i]).text('Wind: ' + data.daily[i].wind_speed + 'MPH');
+                  humid[i]= document.getElementById('humid' + i)
+                    $(humid[i]).text('Humidity: ' + data.daily[i].humidity + '%');
+                  var date= document.getElementById('date' + i);
+                  $(date).text(moment().add(i, 'days').format('l'));
                 }
                 })
         })
+});
+/*
+$(searchBut).on('click', function(event) {
+    event.preventDefault();
+      var createButton = document
 })
+*/
