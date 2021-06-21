131f4cd4bc3e6eaac4b6d791ed95fcf9cdb55896
Max <m412tinez@gmail.com>
2021-06-20 19:33:18 -0500

    creating 5- day forecast block in html and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var searchBut= $("#searchBtn");
 var APIKey= 'e16523d04c63d1ae7214ce72c3259465'
 var city= $('#city')
+const icons= $('#currentIcon')
+const temp= $('#temp');
+const wind= $('#wind');
+const humid= $('#humid');
+const uv= $('#uv');
 
 $(searchBut).on('click', function (event) {
     event.preventDefault();
@@ -12,13 +17,22 @@ $(searchBut).on('click', function (event) {
             console.log(data);
             var long = data[0].lon;
             var lat = data[0].lat;
-            var searchWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid=' + APIKey;
+            var WeatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid=' + APIKey;
             $(city).text(data[0].name + ', ' + data[0].state)
 
-            fetch(searchWeather)
+            fetch(WeatherAPI)
                 .then(response => response.json())
                 .then(function (data) {
                     console.log(data);
+                    $(icons).attr("src", 'https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png');
+                    $(uv).text('UV index: ' + data.current.uvi);
+                    $(temp).text('Temp: ' + data.current.temp + '\u00B0' + 'F');
+                    $(wind).text('Wind: ' + data.current.wind_speed + 'MPH');
+                    $(humid).text('Humidity: ' + data.current.humidity + '%');
+
+                for(i = 1; i < 6; i++) {
+                    var 
+                }
                 })
         })
 })
